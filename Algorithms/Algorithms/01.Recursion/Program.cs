2d0c083a9207681e7e9a542414c300b66c09c99c
KrumK <kkereziev@abv.bg>
2020-01-17 22:35:15 +0200

    .

---
+++ Algorithms/Algorithms/01.Recursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result=SumArray(5);
+            Console.WriteLine(result);
+            
+        }
+
+        public static int SumArray(int a)
+        {
+           
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 01.Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursion();
+        }
+    }
+}
